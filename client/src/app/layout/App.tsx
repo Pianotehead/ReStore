08e770aadda70d37311a30c3b88f74e6f55f9a0e
Theodor Nordkvist <teddin2@outlook.com>
2021-12-19 02:11:39 +0100

    Organize folders, first hooks/state, Add Material UI

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { Product } from "../models/product";
+import Catalog from "../../features/catalog/catalog";
+import { Typography } from "@mui/material";
+
+
+function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data))
+  }, []);
+
+  function addProduct() {
+    setProducts(prevState => [...prevState,
+      {
+        id: prevState.length + 101,
+        name: 'product' + (prevState.length + 1), 
+        price: prevState.length * 100 + 100,
+        brand: 'some brand',
+        description: 'some description',
+        pictureUrl: 'http://picsum.photos/200'
+      }]);
+  }
+
+  return (
+    <>
+      <Typography variant='h1'>Re-store</Typography>
+      <Catalog products={products} addProduct={addProduct} />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/features/catalog/catalog.tsx
@@ -0,0 +1,30 @@
+import { List, ListItem, ListItemAvatar, 
+    Avatar, ListItemText, Button } from "@mui/material";
+import { Product } from "../../app/models/product";
+
+interface Props {
+    products: Product[];
+    addProduct: () => void;
+}
+
+export default function Catalog({products, addProduct}: Props) {
+    return(
+        <>
+            <h1>Catalog</h1>
+            <List>
+                {products.map(product => (
+                <ListItem key={product.id} >
+                   <ListItemAvatar>
+                       <Avatar src={product.pictureUrl} />
+                   </ListItemAvatar>
+                   <ListItemText>
+                       {product.name} - {product.price}
+                   </ListItemText>
+                </ListItem>
+                ))}
+            </List>
+            <Button variant='contained' onClick={addProduct}>Add product</Button>
+        
+        </>
+    )
+}
