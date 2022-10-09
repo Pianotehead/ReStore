ba4aa2e8c99cc46bca99bc04bff35f4bb16ada58
Theodor Nordkvist <teddin2@outlook.com>
2022-10-09 15:54:47 +0200

    Finish Section 9

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -21,84 +22,83 @@ namespace API.Controllers
 
       [HttpGet(Name = "GetBasket")]
       public async Task<ActionResult<BasketDto>> GetBasket()
-        {
-            var basket = await RetrieveBasket();
+      {
+         var basket = await RetrieveBasket(GetBuyerId());
 
-            if (basket == null) return NotFound();
-            return MapBasketToDto(basket);
-        }
+         if (basket == null) return NotFound();
+         return basket.MapBasketToDto();
+      }
 
 
 
-        [HttpPost] // api/basket?productId=3&quantity=2
+      [HttpPost] // api/basket?productId=3&quantity=2
       public async Task<ActionResult<BasketDto>> AddItemToBasket(int productId, int quantity)
       {
          // 1)   Get basket (or create if does not exist)
-         var basket = await RetrieveBasket();
+         var basket = await RetrieveBasket(GetBuyerId());
          if (basket == null) basket = CreateBasket();
          // 2)   Get product
          var product = await _context.Products.FindAsync(productId);
-         if (product == null) return BadRequest(new ProblemDetails { Title = "Product Not Found"});
+         if (product == null) return BadRequest(new ProblemDetails { Title = "Product Not Found" });
          // 3)   Add item
          basket.AddItem(product, quantity);
          // 4)   Save Changes
          var result = await _context.SaveChangesAsync() > 0;
-         if(result) return CreatedAtRoute("GetBasket", MapBasketToDto(basket));
-         return BadRequest(new ProblemDetails{Title = "Problem saving item to basket"});
+         if (result) return CreatedAtRoute("GetBasket", basket.MapBasketToDto());
+         return BadRequest(new ProblemDetails { Title = "Problem saving item to basket" });
       }
 
       [HttpDelete]
       public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
       {
          // 1)   Get basket
-         var basket = await RetrieveBasket();
+         var basket = await RetrieveBasket(GetBuyerId());
          // 2)   If basket not null => remove item or reduce quantity
          if (basket == null) return BadRequest(new ProblemDetails { Title = "Product Not Found" });
          basket.RemoveItem(productId, quantity);
          // 3)   Save Changes
          var result = await _context.SaveChangesAsync() > 0;
-         if(result) return Ok();
-         return BadRequest(new ProblemDetails{Title = "Problem removing item from the basket"});
+         if (result) return Ok();
+         return BadRequest(new ProblemDetails { Title = "Problem removing item from the basket" });
       }
 
-      private async Task<Basket> RetrieveBasket()
+      private async Task<Basket> RetrieveBasket(string buyerId)
       {
+         if (string.IsNullOrEmpty(buyerId))
+         {
+            Response.Cookies.Delete("buyerId");
+            return null;
+         }
          return await _context.Baskets
              .Include(i => i.Items)
              .ThenInclude(p => p.Product)
-             .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+             .FirstOrDefaultAsync(x => x.BuyerId == buyerId);
+      }
+
+      private string GetBuyerId()
+      {
+         return User.Identity?.Name ?? Request.Cookies["buyerId"];
       }
 
       private Basket CreateBasket()
       {
-        var buyerId = Guid.NewGuid().ToString();
-        var cookieOptions = new CookieOptions
-        {
-            IsEssential = true, Expires = DateTime.Now.AddDays(30)
-        };
-        Response.Cookies.Append("buyerId", buyerId, cookieOptions);
-        var basket = new Basket{BuyerId = buyerId};
-        _context.Baskets.Add(basket);
-        return basket;
-      }
-    private static BasketDto MapBasketToDto(Basket basket)
-    {
-        return new BasketDto
-        {
-            Id = basket.Id,
-            BuyerId = basket.BuyerId,
-            Items = basket.Items.Select(item => new BasketItemDto
+         var buyerId = User.Identity?.Name;
+         if (string.IsNullOrEmpty(buyerId))
+         {
+            buyerId = Guid.NewGuid().ToString();
+            var cookieOptions = new CookieOptions
             {
-                ProductId = item.ProductId,
-                Name = item.Product.Name,
-                Price = item.Product.Price,
-                PictureUrl = item.Product.PictureUrl,
-                Type = item.Product.Type,
-                Brand = item.Product.Brand,
-                Quantity = item.Quantity
-            }).ToList()
-        };
-    }
+                IsEssential = true,
+                Expires = DateTime.Now.AddDays(30)
+            };
+            Response.Cookies.Append("buyerId", buyerId, cookieOptions);
+         }
+         
+         var basket = new Basket { BuyerId = buyerId };
+         _context.Baskets.Add(basket);
+         return basket;
+      }
+      
 
    }
 }
--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
 import { Container, createTheme, CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Route, Switch} from "react-router-dom";
 import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
@@ -13,29 +13,33 @@ import 'react-toastify/dist/ReactToastify.css';
 import ServerError from "../errors/ServerError";
 import NotFound from "../errors/NotFound";
 import BasketPage from "../../features/basket/BasketPage";
-import { getCookie } from "../util/util";
-import agent from "../api/agent";
 import CheckoutPage from "../../features/checkout/CheckoutPage";
 import LoadingComponent from "./LoadingComponent";
 import { useAppDispatch } from "../store/configureStore";
-import { setBasket } from "../../features/basket/basketSlice";
+import { fetchBasketAsync } from "../../features/basket/basketSlice";
+import Login from "../../features/account/Login";
+import Register from "../../features/account/Register";
+import { fetchCurrentUser } from "../../features/account/accountSlice";
+import PrivateRoute from "./PrivateRoute";
 
 
 function App() {
   const dispatch = useAppDispatch();
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const buyerId = getCookie('buyerId');
-    if (buyerId) {
-      agent.Basket.get()
-        .then(basket => dispatch(setBasket(basket)))
-        .catch(error => console.log(error))
-        .finally(() => setLoading(false));
-    } else {
-      setLoading(false);
+  const initApp = useCallback(async () => {
+    try {
+      await dispatch(fetchCurrentUser());
+      await dispatch(fetchBasketAsync());
+    } catch (error) {
+      console.log(error);
     }
   }, [dispatch])
+
+  useEffect(() => {
+    initApp().then(() => setLoading(false));
+  }, [initApp])
+
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark' : 'light';
   const theme = createTheme({
@@ -67,7 +71,9 @@ function App() {
           <Route path='/contact' component={ContactPage} />
           <Route path='/server-error' component={ServerError} />
           <Route path='/basket' component={BasketPage} />
-          <Route path='/checkout' component={CheckoutPage} />
+          <PrivateRoute path='/checkout' component={CheckoutPage} />
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
           <Route component={NotFound} />
         </Switch>
       </Container>
